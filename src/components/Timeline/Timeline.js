8f4610c1a1c667719be605c8cfcfd6c9e97c435e
unknown <albertramadhanvanwijk@gmail.com>
2021-11-18 00:58:54 +0700

    Timeline Component

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -5,32 +5,41 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import WorkIcon from '@mui/icons-material/Work';
 
-const CustomTimeline = () => {
+import './Timeline.css';
+import { Typography } from '@material-ui/core';
+import { render } from '@testing-library/react';
+
+const CustomTimeline = ({title, children}) => {
     return (
-        <Timeline>
-      <TimelineItem>
+      <Timeline className={'timeline'}>
+      { /* Item Header */ }
+      <TimelineItem className={'timeline_firstItem'}>
         <TimelineSeparator>
-          <TimelineDot />
+          <TimelineDot className={'timeline_dot_header'}>
+            {WorkIcon}
+          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>
+          <Typography variant='h6' className={'timeline_header'}>
+            {title}
+          </Typography>
+        </TimelineContent>
       </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
+      {children}
     </Timeline>
-    )
-}
+    );
+};
+
+export const CustomTimelineSeparator = () => {
+  return (
+    <TimelineSeparator className={'separator_padding'}>
+      <TimelineDot variant={'outlined'} className={'timeline_dot'} />
+      <TimelineConnector />
+    </TimelineSeparator>
+  );
+};
 
 export default CustomTimeline;
